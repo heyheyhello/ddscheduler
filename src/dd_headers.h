c7cf05ce0359ad58956c98dab3bf2d87bb322719
Gen Hames <hamesg@tutanota.de>
2021-04-17 01:58:20 -0700

    Use heap_4.c and update linked list headers

--- src/dd_headers.h
+++ src/dd_headers.h
@@ -8,33 +8,35 @@
 // Includes copied from FreeRTOS Project 0
 
 /* Standard includes. */
+#include "stm32f4_discovery.h"
 #include <stdint.h>
 #include <stdio.h>
-#include "stm32f4_discovery.h"
 
 /* Kernel includes. */
-#include "stm32f4xx.h"
 #include "../FreeRTOS_Source/include/FreeRTOS.h"
 #include "../FreeRTOS_Source/include/queue.h"
 #include "../FreeRTOS_Source/include/semphr.h"
 #include "../FreeRTOS_Source/include/task.h"
 #include "../FreeRTOS_Source/include/timers.h"
+#include "../FreeRTOS_Source/portable/MemMang/heap_4.c"
+#include "stm32f4xx.h"
 
-typedef enum DD_Task_Enum_t
-{
-  PERIODIC,
-  APERIODIC
-} DD_Task_Enum_t;
+// From our PDF doc. Everything is on GPIOC.
+// Red/Amber/Green
+#define TRAFFIC_LIGHT_R GPIO_Pin_0
+#define TRAFFIC_LIGHT_A GPIO_Pin_1
+#define TRAFFIC_LIGHT_G GPIO_Pin_2
 
-typedef enum DD_Message_Enum_t
-{
-  DD_API_Message_Task_Create,
-  DD_API_Message_Task_Delete,
-  DD_API_Message_Fetch_Task_List,
+typedef enum DD_Task_Enum_t { PERIODIC, APERIODIC } DD_Task_Enum_t;
+
+typedef enum DD_Message_Enum_t {
+  DD_API_Req_Task_Create,
+  DD_API_Req_Task_Delete,
+  DD_API_Req_Fetch_Task_List,
+  DD_API_Res_OK,
 } DD_Message_Enum_t;
 
-typedef struct DD_Task_t
-{
+typedef struct DD_Task_t {
   TaskHandle_t task_handle;
   DD_Task_Enum_t type;
   uint32_t id;
@@ -43,35 +45,36 @@ typedef struct DD_Task_t
   uint32_t completion_time;
 } DD_Task_t;
 
-typedef struct DD_Message_t
-{
+typedef struct DD_Message_t {
   DD_Message_Enum_t type;
   void *data;
 } DD_Message_t;
 
 // Linked list implementation
 
-typedef struct DD_LL_Leader_t
-{
+typedef struct DD_LL_Leader_t {
   DD_LL_Node_t *head;
   DD_LL_Node_t *cursor;
   DD_LL_Node_t *cursor_prev;
   unsigned int length;
 } DD_LL_Leader_t;
 
-typedef struct DD_LL_Node_t
-{
+typedef struct DD_LL_Node_t {
   DD_Task_t *task;
   struct DD_LL_Node_t *next;
 } DD_LL_Node_t;
 
 DD_LL_Leader_t *ll();
 DD_LL_Node_t *ll_node(DD_Task_t *dd_task);
-void ll_next(DD_LL_Leader_t *leader);
-void ll_cursor_prepend(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add);
-void ll_cursor_append(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add);
-DD_LL_Node_t *ll_cursor_unlink(DD_LL_Leader_t *leader);
+void ll_cur_head(DD_LL_Leader_t *leader);
+void ll_cur_next(DD_LL_Leader_t *leader);
+void ll_cur_tail(DD_LL_Leader_t *leader);
+void ll_cur_prepend(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add);
+void ll_cur_append(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add);
+DD_LL_Node_t *ll_cur_unlink(DD_LL_Leader_t *leader);
 void ll_print(DD_LL_Leader_t *leader);
 void ll_empty(DD_LL_Leader_t *leader);
 
+void DD_Scheduler_Task(void *pvParameters);
+void DD_Monitor_Task(void *pvParameters);
 #endif
