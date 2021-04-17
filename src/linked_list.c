5f2ab2bfb072b2c43efbe8e8820bb7bf3d2d1093
Gen Hames <hamesg@tutanota.de>
2021-04-17 01:57:09 -0700

    Touch up linked list and switch to pvMalloc. No need to do malloc checks

--- src/linked_list.c
+++ src/linked_list.c
@@ -4,13 +4,7 @@
 
 DD_LL_Leader_t *ll_leader()
 {
-  DD_LL_Leader_t *leader = (DD_LL_Leader_t *)malloc(sizeof(DD_LL_Leader_t));
-  if (!leader)
-  {
-    printf("Error trying to malloc in ll_leader");
-    return NULL;
-  }
-
+  DD_LL_Leader_t *leader = (DD_LL_Leader_t *)pvPortMalloc(sizeof(DD_LL_Leader_t));
   leader->head = NULL;
   leader->cursor = NULL;
   leader->cursor_prev = NULL;
@@ -20,19 +14,19 @@ DD_LL_Leader_t *ll_leader()
 
 DD_LL_Node_t *ll_node(DD_Task_t *dd_task)
 {
-  DD_LL_Node_t *item = (DD_LL_Node_t *)malloc(sizeof(DD_LL_Node_t));
-  if (!item)
-  {
-    printf("Error trying to malloc in ll_node");
-    return NULL;
-  }
-
+  DD_LL_Node_t *item = (DD_LL_Node_t *)pvPortMalloc(sizeof(DD_LL_Node_t));
   item->task = dd_task;
   item->next = NULL;
   return item;
 }
 
-void ll_next(DD_LL_Leader_t *leader)
+void ll_cur_head(DD_LL_Leader_t *leader)
+{
+  leader->cursor = leader->head;
+  leader->cursor_prev = NULL;
+}
+
+void ll_cur_next(DD_LL_Leader_t *leader)
 {
   if (leader->cursor == NULL)
     return;
@@ -40,54 +34,40 @@ void ll_next(DD_LL_Leader_t *leader)
   leader->cursor = leader->cursor->next;
 }
 
-// Don't use these. Always use a cursor...
+void ll_cur_tail(DD_LL_Leader_t *leader)
+{
+  for (ll_cur_head(leader); leader->cursor->next; ll_cur_next(leader))
+    ;
+}
 
-// void ll_prepend(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add) {
-//   to_add->next = leader->head;
-//   leader->head = to_add;
-//   leader->length++;
-// }
-
-// void ll_append(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add) {
-//   if (leader->head == NULL) {
-//     leader->head = to_add;
-//   } else {
-//     /* Move to end of list */
-//     DD_LL_Node_t *runner = leader->head;
-//     for (; runner->next; runner = runner->next) {}
-//     runner->next = to_add;
-//   }
-//   leader->length++;
-// }
-
-void ll_cursor_prepend(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add)
+void ll_cur_prepend(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add)
 {
   if (leader->cursor == NULL)
   {
-    printf("ll_cursor_prepend cursor is NULL\n");
-    free(to_add);
+    printf("ll_cur_prepend cursor is NULL\n");
     return;
   }
   to_add->next = leader->cursor;
   if (leader->cursor == leader->head)
   {
-    /* leader->cursor_prev would be null so ->next would crash */
+    // leader->cursor_prev would be null so ->next would crash
     leader->head = to_add;
   }
   else
   {
+    if (leader->cursor_prev->next != leader->cursor)
+      printf("bug: ll_cur_prepend cursor_prev isn't previous of cursor");
     leader->cursor_prev->next = to_add;
   }
   leader->cursor_prev = to_add;
   leader->length++;
 }
 
-void ll_cursor_append(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add)
+void ll_cur_append(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add)
 {
   if (leader->cursor == NULL)
   {
-    printf("ll_cursor_append cursor is NULL\n");
-    free(to_add);
+    printf("bug: ll_cur_append cursor is NULL\n");
     return;
   }
   to_add->next = leader->cursor->next;
@@ -95,26 +75,28 @@ void ll_cursor_append(DD_LL_Leader_t *leader, DD_LL_Node_t *to_add)
   leader->length++;
 }
 
-DD_LL_Node_t *ll_cursor_unlink(DD_LL_Leader_t *leader)
+DD_LL_Node_t *ll_cur_unlink(DD_LL_Leader_t *leader)
 {
   if (leader->cursor == NULL)
   {
-    printf("ll_cursor_unlink cursor is NULL\n");
+    printf("bug: ll_cur_unlink cursor is NULL\n");
     return NULL;
   }
   if (leader->cursor == leader->head)
   {
-    /* leader->cursor_prev would be null so ->next would crash */
+    // leader->cursor_prev would be null so ->next would crash
     leader->head = leader->cursor->next;
   }
   else
   {
+    if (leader->cursor_prev->next != leader->cursor)
+      printf("bug: ll_cur_unlink cursor_prev isn't previous of cursor");
     leader->cursor_prev->next = leader->cursor->next;
   }
   leader->cursor->next = NULL;
   DD_LL_Node_t *item = leader->cursor;
   // Don't make an assumption of what the cursor should point at next. They have
-  // the prev_cursor to reset the cursor if they need ot unlink in a loop.
+  // the prev_cursor to reset the cursor if they need to unlink in a loop.
   leader->cursor = NULL;
   leader->length--;
   return item;
@@ -124,13 +106,13 @@ void ll_print(DD_LL_Leader_t *leader)
 {
   if (leader == NULL)
   {
-    printf("ll_print leader is NULL\n");
+    printf("bug: ll_print leader is NULL\n");
     return;
   }
   DD_LL_Node_t *runner = leader->head;
   DD_Task_t *task;
 
-  printf("LL: list: items: %d\n", leader->length);
+  printf("ll_print items: %d\n", leader->length);
   for (; runner; runner = runner->next)
   {
     task = runner->task;
@@ -142,15 +124,14 @@ void ll_print(DD_LL_Leader_t *leader)
   printf("\n");
 }
 
-// This is more efficient that looping ll_cursor_unlink
+// This is more efficient that looping ll_cur_unlink
 void ll_empty(DD_LL_Leader_t *leader)
 {
-  leader->cursor = leader->head;
-  leader->cursor_prev = NULL;
+  ll_cur_head(leader);
   while (leader->length)
   {
-    ll_next(leader);
-    free(leader->cursor_prev);
+    ll_cur_next(leader);
+    vPortFree(leader->cursor_prev);
     leader->length--;
   }
   leader->cursor = leader->head = leader->cursor_prev = NULL;
